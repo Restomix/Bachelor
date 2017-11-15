b6346a1d2076ebc854cd45bd96df3876235876e4
Yrii <novostragonlakos@gmail.com>
2017-11-15 17:19:29 +0200

    Add some new Logic to Track controller

--- Bacheler work/Controllers/TrackController.cs
+++ Bacheler work/Controllers/TrackController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bacheler_work.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,11 @@ namespace Bacheler_work.Controllers
         public ActionResult Index()
         {
             return View("Main");
+        }
+        [HttpPost]
+        public ActionResult AddSensor(Sensor one)
+        {
+            return View(one);
         }
     }
 }
